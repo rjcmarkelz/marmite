15f50e3fce1def091a62842acc8b8a04484d421f
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-01-19 16:06:59 -0800

    quick plots for lab meeting

--- scripts/brassica_BLUPs.R
+++ scripts/brassica_BLUPs.R
@@ -71,6 +71,8 @@ br_phys
 
 #about a minute
 br_phys <- calc.errorlod(br_phys, error.prob=0.001)
+#
+br_phys
 pheno_list <- pull.pheno(br_phys)
 colnames(pheno_list)
 
@@ -79,15 +81,73 @@ so_flowering <- scanone(br_phys, pheno.col = 4,
 plot(so_flowering)
 
 so_flowering <- scanone(br_phys, pheno.col = "germ_flr", method = "imp", use="all.obs")
-plot(so_flowering)
+plot(so_flowering, chr = "A10")
 
 so_X2012_UN_r <- scanone(br_phys, pheno.col = "X2012_UN_r", method = "imp", use="all.obs")
 plot(so_X2012_UN_r)
 
+#potential overlapping eQTL hotspots
+so_X2011_STP <- scanone(br_phys, pheno.col = "X2011_STP", method = "imp", use="all.obs")
+plot(so_X2011_STP, chr = "A03")
+
+so_X2012_CR_STP <- scanone(br_phys, pheno.col = "X2012_CR_STP", method = "imp", use="all.obs")
+plot(so_X2012_CR_STP, chr = "A03")
+
+so_X2012_UN_STP <- scanone(br_phys, pheno.col = "X2012_UN_STP", method = "imp", use="all.obs")
+plot(so_X2012_UN_STP, chr = "A03")
+
+so_X2012_diff_STP <- scanone(br_phys, pheno.col = "X2012_diff_STP", method = "imp", use="all.obs")
+plot(so_X2012_diff_STP, chr = "A03")
+
+save.image(file = "brassica_BLUPs.RData", version = NULL,
+ ascii = FALSE, safe = TRUE)
+so.perm2 <- scanone(br_phys, method = "imp", n.perm = 1000) 
+summary(so.perm2)
+so.perm295 <- summary(so.perm2)[1] #keep 95%
+# LOD thresholds (1000 permutations)
+#      lod
+# 5%  3.23
+# 10% 2.81
+
+#old new compare
+brassica_traits <- read.cross("csvsr", genfile ="old_map_rqtl_missing_RILS_removed.csv", 
+	                       phefile="br_blups_RQTL.csv", genotypes=c("0","2"))
+
+class(brassica_traits)[1] <- "riself"
+brassica_traits <- jittermap(brassica_traits)
+brassica_traits
+traits_list <- pull.pheno(brassica_traits)
+colnames(traits_list)
+
+X2011_STP
+scanone_2011_STP <- scanone(brassica_traits, pheno.col = "X2011_STP", method = "imp", use="all.obs")
+scanone_2011_STP
+plot(scanone_2011_STP, chr = 3)
+
+so.perm <- scanone(brassica_traits, method = "imp", n.perm = 1000) 
+summary(so.perm)
+so.perm95 <- summary(so.perm)[1] #keep 95%
+# LOD thresholds (1000 permutations)
+#      lod
+# 5%  2.44
+# 10% 2.13
 
 
 
 
 
+qtl_plot <- qtl_plot +        facet_grid(~ chr, scales = "free_x", space = "free_x") +
+geom_rect(aes(fill = background.color), xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf) +
+                        geom_line(aes(x = pos, y = lod), size = 2) +
+                        geom_hline(yintercept = 0.50, color = "red", size = 1) +
+                        geom_segment(aes(x = pos, xend = pos), y = (peak * -0.02), yend = (peak * -0.05)) +
+                        scale_y_continuous(expand = c(0, 0), limits = c((peak * -0.06), (peak * 1.02))) +
+                        theme(legend.position = "none",
+                          axis.text.x = element_text(angle = 90),
+                          axis.line=element_line(),
+                          panel.margin = unit(0, "cm")) +
+                        ggtitle("LOD Curves for QTLs") +
+                        xlab("Position in cM") +
+                        ylab("LOD Score") 
 
 
--- scripts/un_eqtl.R
+++ scripts/un_eqtl.R
@@ -50,9 +50,58 @@ high1
 hots1 <- hotsize(high1, lod.thr = lod.thr)
 hots1
 
-plot(hots1, cex.lab = 1.5, cex.axis = 1.5)
+plot(hots1, cex.lab = 1.5, cex.axis = 1.5, chr = "A03")
+
+library(eqtl)
+?define.peak
+brassica_peaks <- define.peak(scanone_imp_tot, th = 4.0, si = 1.0,
+                               lodcolumn= "all", chr=c("A03"))
+attributes(brassica_peaks)$scanone
 
 
+tail(brassica_peaks, 20)
+dim(brassica_peaks)
 
 
+# get significant peaks
+brassica_peaks[2][1]
+is.na(brassica_peaks[2][1])
+
+
+test <- brassica_peaks[1:10]
+class(test)
+head(test)
+?pull.pheno
+testpheno <- colnames(pull.pheno(brass_total, pheno.col = 1:10))
+testpheno
+
+test2 <- as.data.frame(is.na(sapply(test,`[`,1)))
+str(test2)
+test2
+
+test
+test3 <- test[!is.na(sapply(test,`[`,1))]
+str(test3)
+
+# QUICK AND DIRTY, build back up peak object
+attributes(test3)$class <- c("peak", "list")
+attributes(test3)$features <- c("lod", "mname.peak", "peak.cM", 
+        "mname.inf", "inf.cM", "mname.sup", "sup.cM", "si.quality")
+attributes(test3)$scanone <- "scanone_imp_tot"
+attributes(test3)$lod.th <- 4.0
+attributes(test3)$si <- 1.0
+attributes(test3)$window <- 20
+attributes(test3)
+# got it
+
+peaks_red <- brassica_peaks[!is.na(sapply(brassica_peaks,`[`,1))]
+attributes(peaks_red)$class <- c("peak", "list")
+attributes(peaks_red)$features <- c("lod", "mname.peak", "peak.cM", 
+        "mname.inf", "inf.cM", "mname.sup", "sup.cM", "si.quality")
+attributes(peaks_red)$scanone <- "scanone_imp_tot"
+attributes(peaks_red)$lod.th <- 4.0
+attributes(peaks_red)$si <- 1.0
+attributes(peaks_red)$window <- 20
+attributes(peaks_red)
+length(peaks_red)
 
