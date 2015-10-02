172f6573978518276387c248a2174f7d33004d17
Cody Markelz <rjmarkelz@ucdavis.edu>
2015-10-02 09:33:56 -0700

    gxe and gene subsets

--- scripts/cr_un_eqtl.R
+++ scripts/cr_un_eqtl.R
@@ -42,6 +42,7 @@ system.time(scanone.imp.1 <- scanone(brassica_genes, pheno.col = 1:35039,
 
 head(scanone.imp.1)
 
+brassica_genes <- calc.genoprob(brassica_genes)
 
 # save output in data directory
 save.image(file = "cr_un_eqtl.RData", version = NULL,
@@ -63,10 +64,19 @@ lod.thrs
 lod.thr <- lod.thrs[5]
 
 #reduce object size by getting removing NS peaks
+?highlod
 high1 <- highlod(scanone.imp.1, lod.thr = lod.thr, drop.lod = 1.5)
 max(high1, lod.thr = lod.thrs)
 high1
 hots1 <- hotsize(high1, lod.thr = lod.thr)
+high1
+
+high_9 <- highlod(scanone.imp.1, chr = "A09",lod.thr = lod.thr, drop.lod = 1.5 )
+high_9
+
+
+high_9 <- pull.highlod(high1, chr = A09, pos =68.0)
+high_9
 high1
 summary(hots1)
 # LOD threshold: 3.125673 
@@ -110,96 +120,174 @@ save.image(file = "cr_un_eqtl.RData", version = NULL,
 
 
 # cis trans analysis
-AO1_eqtl <- highlod(scanone.imp.1, chr = "A01" , lod.thr = lod.thr, drop.lod = 1.5)
-str(AO1_eqtl)
-highlodA01 <- as.data.frame(AO1_eqtl$highlod)
-head(highlodA01)
-dim(highlodA01)
-
-summary(scanone.imp.1)
-system.time(brassica_genes_sig <- as.data.frame(summary(scanone.imp.1, 
-	                          threshold = 3.15, format = "allpeaks" )))
-
-system.time(scanone.imp.4 <- scanone(brassica_genes, pheno.col = 1:100,
- method = "imp", use="all.obs"))
-system.time(brassica_genes_sig <- as.data.frame(summary(scanone.imp.4, 
-	                          threshold = 3.15, format = "allpeaks" )))
-head(brassica_genes_sig)
-
-
-library(eqtl)
-#define the peaks from the scanone object
-brassica_peaks <- define.peak(scanone.imp.1, lodcolumn= 'all', th = 3.5, si = 1.5)
-class(brassica_peaks)
-attributes(brassica_peaks)
-summary(brassica_peaks)
-str(brassica_peaks)
-
-define.peak(scanone.imp.1, th = 3.5, lodcolumn= 1:35000, graph = TRUE, chr=c("A01"));
-
-# make a copy to modify
+?highlod
 str(scanone.imp.1)
-scanone.imp.2 <- scanone.imp.1
-str(scanone.imp.2)
+scanone.imp.1[2:3,]
+head(rownames(scanone.imp.1))
+tail(rownames(scanone.imp.1))
+summary(scanone.imp.1)
 
-scanone.imp.2$chr <- as.numeric(sub("(A)(.+)","\\2", scanone.imp.2$chr))
-scanone.imp.2$chr
+scanone_rows <- as.data.frame(rownames(scanone.imp.1))
+head(scanone_rows)
+rownames(scanone_rows)
+scanone_rows$index <- as.numeric(rownames(scanone_rows))
+head(scanone_rows)
+#rownames of scanone objects are marker names
 
-head(scanone.imp.1)
+A01_eqtl <- highlod(scanone.imp.1, chr = "A01" , lod.thr = lod.thr, drop.lod = 0.1, restrict.lod = TRUE)
+str(A01_eqtl)
+max(A01_eqtl$highlod$row)
+
+# pull eqtl
+highlod_out <- as.data.frame(A01_eqtl$highlod)
+max(highlod_out$lod)
+#[1] 6.632117
+head(highlod_out)
+dim(highlod_out)
+
+# pull marker indexs 
+head(scanone_rows)
+
+# pull gene indexs
+gene_indx <- as.data.frame(A01_eqtl$names)
+head(gene_indx)
+gene_indx$gene_index <- as.numeric(rownames(gene_indx))
+
+highlod_marker <- merge(highlod_out, scanone_rows, by.x = "row", by.y = "index", all.x = TRUE)
+dim(highlod_marker)
+head(highlod_marker)
+
+highlod_marker_gene <- merge(highlod_marker, gene_indx, by.x = "phenos", by.y = "gene_index", all.x = TRUE)
+dim(highlod_marker)
+head(highlod_marker_gene)
+colnames(highlod_marker_gene)[4:5] <- c("marker_names", "gene_names")
+
+# might as well include the marker genetic map data
+# pull gene indexs
+marker_indx <- as.data.frame(A01_eqtl$chr.pos)
+head(marker_indx)
+marker_indx$marker_index <- as.character(rownames(marker_indx))
 
 
-head(ATH.coord)
-str(ATH.coord)
-#infile genome data
-#used the BEDtoGFF.pl script to convert the BED file to GTF
-setwd("/Users/Cody_2/git.repos/brassica_genome_db/raw_data")
-# update with new genome mapping
-br_bed <- read.table("Brassica_rapa_v1.5_final.bed", header = FALSE)
-head(br_bed)
-str(br_bed)
-
-#transcripts data frame
-transcripts <- br_bed[,c("V4","V4","V1","V6","V2","V3")]
-head(transcripts, 10)
-tail(transcripts, 20)
-
-#make unique id for each gene based on row number for now 1-44239
-transcripts$V4 <- rownames(transcripts)
-head(transcripts)
-
-#replace names of new 
-colnames(transcripts) <- c("tx_id", "tx_name", "tx_chrom", "tx_strand",
-	                         "tx_start", "tx_end")
-head(transcripts)
-str(transcripts)
+gxe_genes_markers <- merge(highlod_marker_gene, marker_indx, by.x = "marker_names", by.y = "marker_index", all.x = TRUE)
+head(gxe_genes_markers)
 
 setwd("/Users/Cody_2/git.repos/brassica_eqtl_v1.5/data")
 # write table for eQTL cis/trans
-write.table(transcripts, "transcripts_eqtl_start_stop_eqtl.csv", sep = ",", col.names = TRUE, row.names = FALSE)
-
-#format for eQTL
-transcripts <- transcripts[c(2,3,5,6)]
-head(transcripts)
-tail(transcripts)
-
-transcripts$tx_chrom  <- sub("(A0)(\\d)","\\2", transcripts$tx_chrom)
-transcripts$tx_chrom  <- sub("(A10)","10", transcripts$tx_chrom)
-transcripts <- transcripts[!grepl("Scaff", transcripts$tx_chrom),]
-colnames(transcripts) <- c("etrait.name", "chr", "start", "stop")
-str(transcripts)
-transcripts$etrait.name <- as.character(transcripts$etrait.name)
-transcripts$chr <- as.numeric(transcripts$chr)
-
-######
-#
-######
-brass_local <- localize.qtl(cross = scanone.imp.1, peak = brassica_peaks, data.gmap = transcripts);
-
+write.table(gxe_genes_markers, "gxe_genes_markers.csv", sep = ",", col.names = TRUE, row.names = FALSE)
 
 # save output in data directory
 setwd("/Users/Cody_2/git.repos/brassica_eqtl_v1.5/data")
-save.image(file = "cr_un_eqtl.RData", version = NULL,
+save.image(file = "brassica_genes_sig.RData", version = NULL,
  ascii = FALSE, safe = TRUE)
+
+# now for some plotting
+dim(gxe_genes_markers)
+
+library(ggplot2)
+
+gxe_plot <- ggplot(gxe_genes_markers)
+gxe_plot <- gxe_plot +  theme_bw() + geom_point(aes(x = pos, y = lod,  alpha = 0.1), size = 2) +
+                        facet_grid(chr ~ .) + 
+                        ggtitle("G by E eQTL distribution") +
+                        xlab("Genetic Position (cM)") +
+                        ylab("LOD Score") +
+                        theme(axis.title=element_text(face="bold",size="20"),
+                              axis.text=element_text(face="bold", size="12"),
+                              legend.position = "none")  
+gxe_plot
+setwd("/Users/Cody_2/git.repos/brassica_eqtl_v1.5/output")
+ggsave(gxe_plot, file = "gxe_hotspots.pdf", width = 10, height = 20 )
+
+
+#####
+# did not end up using this
+#####
+# summary(scanone.imp.1)
+# system.time(brassica_genes_sig <- as.data.frame(summary(scanone.imp.1, 
+# 	                          threshold = 3.15, format = "allpeaks" )))
+
+# system.time(scanone.imp.4 <- scanone(brassica_genes, pheno.col = 1:100,
+#  method = "imp", use="all.obs"))
+# system.time(brassica_genes_sig <- as.data.frame(summary(scanone.imp.4, 
+# 	                          threshold = 3.15, format = "allpeaks" )))
+# head(brassica_genes_sig)
+
+# library(eqtl)
+# #define the peaks from the scanone object
+# brassica_peaks <- define.peak(scanone.imp.1, lodcolumn= 'all', th = 3.5, si = 1.5)
+# class(brassica_peaks)
+# attributes(brassica_peaks)
+# summary(brassica_peaks)
+# str(brassica_peaks)
+
+# define.peak(scanone.imp.1, th = 3.5, lodcolumn= 1:35000, graph = TRUE, chr=c("A01"));
+
+# # make a copy to modify
+# str(scanone.imp.1)
+# scanone.imp.2 <- scanone.imp.1
+# str(scanone.imp.2)
+
+# scanone.imp.2$chr <- as.numeric(sub("(A)(.+)","\\2", scanone.imp.2$chr))
+# scanone.imp.2$chr
+
+# head(scanone.imp.1)
+
+
+# head(ATH.coord)
+# str(ATH.coord)
+
+# #infile genome data
+# #used the BEDtoGFF.pl script to convert the BED file to GTF
+# setwd("/Users/Cody_2/git.repos/brassica_genome_db/raw_data")
+# # update with new genome mapping
+# br_bed <- read.table("Brassica_rapa_v1.5_final.bed", header = FALSE)
+# head(br_bed)
+# str(br_bed)
+
+# #transcripts data frame
+# transcripts <- br_bed[,c("V4","V4","V1","V6","V2","V3")]
+# head(transcripts, 10)
+# tail(transcripts, 20)
+
+# #make unique id for each gene based on row number for now 1-44239
+# transcripts$V4 <- rownames(transcripts)
+# head(transcripts)
+
+# #replace names of new 
+# colnames(transcripts) <- c("tx_id", "tx_name", "tx_chrom", "tx_strand",
+# 	                         "tx_start", "tx_end")
+# head(transcripts)
+# str(transcripts)
+
+# setwd("/Users/Cody_2/git.repos/brassica_eqtl_v1.5/data")
+# # write table for eQTL cis/trans
+# write.table(transcripts, "transcripts_eqtl_start_stop_eqtl.csv", sep = ",", col.names = TRUE, row.names = FALSE)
+
+# #format for eQTL
+# transcripts <- transcripts[c(2,3,5,6)]
+# head(transcripts)
+# tail(transcripts)
+
+# transcripts$tx_chrom  <- sub("(A0)(\\d)","\\2", transcripts$tx_chrom)
+# transcripts$tx_chrom  <- sub("(A10)","10", transcripts$tx_chrom)
+# transcripts <- transcripts[!grepl("Scaff", transcripts$tx_chrom),]
+# colnames(transcripts) <- c("etrait.name", "chr", "start", "stop")
+# str(transcripts)
+# transcripts$etrait.name <- as.character(transcripts$etrait.name)
+# transcripts$chr <- as.numeric(transcripts$chr)
+
+# ######
+# #
+# ######
+# ?localize.qtl
+# brass_local <- localize.qtl(cross = brassica_genes, peak = brassica_peaks, data.gmap = transcripts)
+# brass_local
+
+# # save output in data directory
+# setwd("/Users/Cody_2/git.repos/brassica_eqtl_v1.5/data")
+# save.image(file = "cr_un_eqtl.RData", version = NULL,
+#  ascii = FALSE, safe = TRUE)
 
 
 
